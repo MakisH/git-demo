e98593ff9efc41978c261120051325a25fca90f3
Gerasimos Chourdakis <makishourdakis@gmail.com>
2019-10-18 14:19:25 +0200

    Add default dimension for cubes
    I just check if the number of arguments is less than 2.

--- cube_info.m
+++ cube_info.m
@@ -6,6 +6,10 @@ function [vol, diag] = cube_info1(e,d)
 %  a cube VOL and the length of diagonal of the cube,
 %  where E is the length of a edge of a D-dimensional cube.
 
+if (nargin < 2),
+    d = 3;
+end
+
 vol = e^d;
 diag = e * sqrt(d);
 
