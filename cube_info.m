57d86a70d77437b7a92dc176d25f272d11c537b3
Gerasimos Chourdakis <makishourdakis@gmail.com>
2019-10-18 13:16:58 +0200

    cube_info.m: Fix language issues in function documentation

--- cube_info.m
+++ cube_info.m
@@ -3,8 +3,8 @@ function [vol, diag] = cube_info1(e,d)
 % a cube.
 %
 %  [VOLL,DIAG] = CUBE INFO(E,D) produces the volume of
-%  a cube VOL and %  the length of diagonal of the cube.
-%  Where E is the length of a edge of a D-dimensional cube.
+%  a cube VOL and the length of diagonal of the cube,
+%  where E is the length of a edge of a D-dimensional cube.
 
 vol = e^d;
 diag = e * sqrt(d);
